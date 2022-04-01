438112c38da18373ef213a347da76e5fb5425900
Andriambeloniarivo <100565870+Andriambeloniarivo@users.noreply.github.com>
2022-04-01 15:14:07 +0100

    Update 3-array-contain.js

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,12 @@
-function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) {
-      return true;
+function arrayContain(array, number) {                                Ligne 1 = 4
+  for (let i = 0; i < array.length; i++) {                            Ligne 2 = 7
+    if (array[i] === number) {                                        Ligne 3 = 4
+      return true;                                                    Ligne 4 = 1
     }
   }
-  return false;
+  return false;                                                       Ligne 7 = 1
 }
 
-module.exports = { arrayContain };
+module.exports = { arrayContain };                                    Ligne 10 = 2
+                                                                T3(n) = 4 + 7r(4+1) + 1 = 1 + r(1) + 1
+                                                                O(n) = n = compléxité linéaire.
