2df9be1fc15a5e352686fb3c217e2c7414ca11e1
Andriambeloniarivo <100565870+Andriambeloniarivo@users.noreply.github.com>
2022-04-01 14:47:38 +0100

    Update 1-slice.js

--- 1-slice.js
+++ 1-slice.js
@@ -7,3 +7,12 @@ function slice(array, start, end) {
 }
 
 module.exports = { slice };
+
+
+
+Ligne 1 = 3
+Ligne 2 = 2
+Ligne 3 = 6
+Ligne 4 = 4
+T1(n) = 3 + 2r(6) + 4
+O(n) = n = compléxité linéaire.
